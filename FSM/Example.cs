82faafe32fdf11c5005ecd6d90f3d4ad025c4741
Wirune Kaewjai <wirune.kae@hotmail.com>
2015-09-22 18:29:10 +0700

    Add Example

---
+++ FSM/Example.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using Devdayo;
+using System;
+
+public class Example : MonoBehaviour
+{
+    Fsm fsm;
+
+	void Start ()
+    {
+        fsm = new Fsm(this);
+        fsm.Go<StateOne>();
+    }
+}
+
+public class StateOne : FsmState
+{
+    public override void OnSubscribe()
+    {
+        base.OnSubscribe();
+        Subscribe<Updater>(OnUpdate);
+    }
+
+    public override void OnUnsubscribe()
+    {
+        base.OnUnsubscribe();
+        Unsubscribe<Updater>(OnUpdate);
+    }
+
+    public override void OnEnter()
+    {
+        Debug.Log("State One: Enter");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("State One: Exit");
+    }
+
+    public void OnUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            fsm.Go<StateTwo>();
+        }
+    }
+}
+
+public class StateTwo : FsmStateBehaviour
+{
+    public override void OnEnter()
+    {
+        Debug.Log("State Two: Enter");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("State Two: Exit");
+    }
+
+    public override void OnFixedUpdate()
+    {
+        base.OnFixedUpdate();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fsm.Go<StateOne>();
+        }
+    }
+}
+
+
