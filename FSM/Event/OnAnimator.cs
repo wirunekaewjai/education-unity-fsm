92317182432678acaa2ec36fccc3aa0aede7c1ef
wirunekaewjai <wirune@Wirunes-MacBook-Air.local>
2015-09-23 13:37:24 +0700

    Add Animator and Transformation Observable

---
+++ FSM/Event/OnAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Devdayo
+{
+	[DisallowMultipleComponent]
+	public class AnimatorIK : FsmObservable<int>
+	{
+		void OnAnimatorIK(int layerIndex)
+		{
+			Notify(layerIndex);
+		}
+	}
+	
+	[DisallowMultipleComponent]
+	public class AnimatorMove : FsmObservable
+	{
+		void OnAnimatorMove()
+		{
+			Notify();
+		}
+	}
+}
---
+++ FSM/Event/OnTransformation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Devdayo
+{
+	[DisallowMultipleComponent]
+	public class TransformChildrenChange : FsmObservable
+	{
+		void OnTransformChildrenChanged()
+		{
+			Notify();
+		}
+	}
+
+	[DisallowMultipleComponent]
+	public class TransformParentChange : FsmObservable
+	{
+		void OnTransformParentChanged()
+		{
+			Notify();
+		}
+	}
+}
