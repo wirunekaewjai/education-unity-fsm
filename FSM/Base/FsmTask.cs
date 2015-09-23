12b923efda75ccf3f76cc07d4cd6eb2c80f93c26
wirunekaewjai <wirune@Wirunes-MacBook-Air.local>
2015-09-23 16:49:05 +0700

    Add Singleton.cs and FsmTask has derived from Singleton Class

--- FSM/Base/FsmTask.cs
+++ FSM/Base/FsmTask.cs
@@ -5,7 +5,7 @@ using System;
 namespace Devdayo
 {
     // I'm a hard-worker
-    internal class FsmTask : MonoBehaviour
+    internal class FsmTask : Singleton<FsmTask>
     {
         // Limit of iteration in one frame.
         private const int Limit = 100;
@@ -28,48 +28,11 @@ namespace Devdayo
 
             // Get some rest and wating for a new called.
         }
-        
-        private void OnDestroy()
-        {
-            queues.Clear();
-            
-            // Clear temporary
-            instance = null;
-        }
 
         // Can enqueue any tasks you want !!!
         public void Enqueue(Action action)
         {
             queues.Enqueue(action);
-        }
-
-        // Only-one instance in program. (singleton)
-        private static FsmTask instance = null;
-        public static FsmTask Instance
-        {
-            get
-            {
-                // Never created ?
-                if(null == instance)
-                {
-                    // Looking for instance.
-                    instance = FindObjectOfType<FsmTask>();
-
-                    // Found ?
-                    if(null == instance)
-                    {
-                        // Just create our GameObject.
-                        GameObject g = new GameObject();
-                        g.hideFlags = HideFlags.HideAndDontSave;
-
-                        // Attach our FsmTask to GameObject.
-                        instance = g.AddComponent<FsmTask>();
-                    }
-                }
-
-                // You Got It !!!
-                return instance;
-            }
         }
     }
 }
